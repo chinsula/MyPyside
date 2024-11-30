55c690716db9077bedbff67912e520a10c1eec05
chins <chinsula81@gmail.com>
2024-11-30 11:08:49 +0500

    d_eventfilter_settings_form_commit 1.0

--- lab2/b_laboratory/d_eventfilter_settings.py
+++ lab2/b_laboratory/d_eventfilter_settings.py
@@ -20,11 +20,34 @@
 
 from PySide6 import QtWidgets
 
+from lab2.b_laboratory.ui import d_eventfilter_settings_form
+
 
 class Window(QtWidgets.QWidget):
 
     def __init__(self, parent=None):
         super().__init__(parent)
+
+        self.ui = d_eventfilter_settings_form.Ui_Form()
+        self.ui.setupUi(self)
+        self.initSignals()
+
+        self.ui.dial.setRange(0, 100)
+        self.ui.dial.setSingleStep(1)
+
+    def initSignals(self):
+        self.ui.btnPlus.clicked.connect(self.onDialClicked)
+        self.ui.dial.valueChanged.connect(self.onDialClicked)
+
+
+    # слоты для подключения
+
+    def onDialClicked(self, position=None):
+        print(position)
+
+
+
+
 
 
 if __name__ == "__main__":
--- lab2/b_laboratory/ui/d_eventfilter_settings_form.py
+++ lab2/b_laboratory/ui/d_eventfilter_settings_form.py
@@ -16,18 +16,22 @@ from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
     QImage, QKeySequence, QLinearGradient, QPainter,
     QPalette, QPixmap, QRadialGradient, QTransform)
 from PySide6.QtWidgets import (QApplication, QComboBox, QDial, QHBoxLayout,
-    QLCDNumber, QSizePolicy, QSlider, QVBoxLayout,
-    QWidget)
+    QLCDNumber, QPushButton, QSizePolicy, QSlider,
+    QSpacerItem, QVBoxLayout, QWidget)
 
 class Ui_Form(object):
     def setupUi(self, Form):
         if not Form.objectName():
             Form.setObjectName(u"Form")
-        Form.resize(241, 179)
-        self.verticalLayout_2 = QVBoxLayout(Form)
+        Form.resize(189, 148)
+        Form.setMinimumSize(QSize(189, 148))
+        Form.setMaximumSize(QSize(328, 269))
+        self.verticalLayout_3 = QVBoxLayout(Form)
+        self.verticalLayout_3.setObjectName(u"verticalLayout_3")
+        self.horizontalLayout_2 = QHBoxLayout()
+        self.horizontalLayout_2.setObjectName(u"horizontalLayout_2")
+        self.verticalLayout_2 = QVBoxLayout()
         self.verticalLayout_2.setObjectName(u"verticalLayout_2")
-        self.horizontalLayout = QHBoxLayout()
-        self.horizontalLayout.setObjectName(u"horizontalLayout")
         self.dial = QDial(Form)
         self.dial.setObjectName(u"dial")
         sizePolicy = QSizePolicy(QSizePolicy.Policy.Preferred, QSizePolicy.Policy.Preferred)
@@ -36,31 +40,58 @@ class Ui_Form(object):
         sizePolicy.setHeightForWidth(self.dial.sizePolicy().hasHeightForWidth())
         self.dial.setSizePolicy(sizePolicy)
 
-        self.horizontalLayout.addWidget(self.dial)
+        self.verticalLayout_2.addWidget(self.dial)
+
+        self.horizontalLayout = QHBoxLayout()
+        self.horizontalLayout.setObjectName(u"horizontalLayout")
+        self.btnPlus = QPushButton(Form)
+        self.btnPlus.setObjectName(u"btnPlus")
+        self.btnPlus.setMinimumSize(QSize(30, 0))
+        self.btnPlus.setMaximumSize(QSize(70, 16777215))
+
+        self.horizontalLayout.addWidget(self.btnPlus)
+
+        self.btnMinus = QPushButton(Form)
+        self.btnMinus.setObjectName(u"btnMinus")
+        self.btnMinus.setMinimumSize(QSize(30, 0))
+        self.btnMinus.setMaximumSize(QSize(70, 16777215))
+
+        self.horizontalLayout.addWidget(self.btnMinus)
+
+
+        self.verticalLayout_2.addLayout(self.horizontalLayout)
+
+
+        self.horizontalLayout_2.addLayout(self.verticalLayout_2)
 
         self.verticalLayout = QVBoxLayout()
         self.verticalLayout.setObjectName(u"verticalLayout")
-        self.comboBox = QComboBox(Form)
-        self.comboBox.setObjectName(u"comboBox")
-
-        self.verticalLayout.addWidget(self.comboBox)
-
         self.lcdNumber = QLCDNumber(Form)
         self.lcdNumber.setObjectName(u"lcdNumber")
 
         self.verticalLayout.addWidget(self.lcdNumber)
 
+        self.comboBox = QComboBox(Form)
+        self.comboBox.setObjectName(u"comboBox")
+        self.comboBox.setMinimumSize(QSize(0, 40))
 
-        self.horizontalLayout.addLayout(self.verticalLayout)
+        self.verticalLayout.addWidget(self.comboBox)
+
+        self.verticalSpacer = QSpacerItem(20, 40, QSizePolicy.Policy.Minimum, QSizePolicy.Policy.Expanding)
+
+        self.verticalLayout.addItem(self.verticalSpacer)
 
 
-        self.verticalLayout_2.addLayout(self.horizontalLayout)
+        self.horizontalLayout_2.addLayout(self.verticalLayout)
+
+
+        self.verticalLayout_3.addLayout(self.horizontalLayout_2)
 
         self.horizontalSlider = QSlider(Form)
         self.horizontalSlider.setObjectName(u"horizontalSlider")
-        self.horizontalSlider.setOrientation(Qt.Horizontal)
+        self.horizontalSlider.setOrientation(Qt.Orientation.Horizontal)
 
-        self.verticalLayout_2.addWidget(self.horizontalSlider)
+        self.verticalLayout_3.addWidget(self.horizontalSlider)
 
 
         self.retranslateUi(Form)
@@ -70,5 +101,7 @@ class Ui_Form(object):
 
     def retranslateUi(self, Form):
         Form.setWindowTitle(QCoreApplication.translate("Form", u"Form", None))
+        self.btnPlus.setText(QCoreApplication.translate("Form", u"+", None))
+        self.btnMinus.setText(QCoreApplication.translate("Form", u"-", None))
     # retranslateUi
 
