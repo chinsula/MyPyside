c2e98ea7c06c936c15594c2e389fcdb8b91fc9bb
chins <chinsula81@gmail.com>
2024-12-09 10:35:29 +0500

    lab3 pointB

---
+++ lab3/b_systeminfo_widget.py
@@ -0,0 +1,72 @@
+"""
+Реализовать виджет, который будет работать с потоком SystemInfo из модуля a_threads
+
+Создавать форму можно как в ручную, так и с помощью программы Designer
+
+Форма должна содержать:
+1. поле для ввода времени задержки
+2. поле для вывода информации о загрузке CPU
+3. поле для вывода информации о загрузке RAM
+4. поток необходимо запускать сразу при старте приложения
+5. установку времени задержки сделать "горячей", т.е. поток должен сразу
+реагировать на изменение времени задержки
+"""
+import traceback
+
+import psutil
+from PySide6 import QtWidgets, QtCore
+
+from a_threads import SystemInfo
+
+
+class Window(QtWidgets.QWidget):
+
+    def __init__(self, parent=None):
+        super().__init__(parent)
+
+        self.initUi()
+        self.__initSignals()
+        self.systemInfo = SystemInfo()
+
+    def initUi(self):
+        """
+        Инициализация Ui
+        :return: None
+        """
+        self.inputDelay = QtWidgets.QLineEdit()
+        self.inputDelay.setPlaceholderText("Введите время задержки")
+
+        self.labelCPU = QtWidgets.QLabel("CPU usage: ")
+        self.labelCPU.setText(f"CPU usage: {psutil.cpu_percent()} %")
+        self.labelRAM = QtWidgets.QLabel("RAM usage: ")
+        self.labelRAM.setText(f"RAM usage: {psutil.virtual_memory().percent} %")
+
+        layout = QtWidgets.QVBoxLayout()
+
+        layout.addWidget(self.inputDelay)
+        layout.addWidget(self.labelCPU)
+        layout.addWidget(self.labelRAM)
+
+        self.setLayout(layout)
+        self.setMinimumSize(100, 100)
+
+    def __initSignals(self):
+
+        self.inputDelay.textChanged.connect(self.onChangeInputDelay)
+        self.thread = SystemInfo()
+        self.thread.started.connect(lambda: print("Поток запущен"))
+        self.thread.finished.connect(lambda: print("Поток остановлен"))
+        self.thread.start()
+
+    def onChangeInputDelay(self):
+        self.systemInfo.delay = int(self.inputDelay.text())
+        print(self.systemInfo.delay)
+
+
+
+
+if __name__ == '__main__':
+    app = QtWidgets.QApplication()
+    window = Window()
+    window.show()
+    app.exec()
---
+++ lab3/c_weatherapi_widget.py
@@ -0,0 +1,96 @@
+"""
+Реализовать виджет, который будет работать с потоком WeatherHandler из модуля a_threads
+
+Создавать форму можно как в ручную, так и с помощью программы Designer
+
+Форма должна содержать:
+1. поле для ввода широты и долготы (после запуска потока они должны блокироваться)
+2. поле для ввода времени задержки (после запуска потока оно должно блокироваться)
+3. поле для вывода информации о погоде в указанных координатах
+4. поток необходимо запускать и останавливать при нажатии на кнопку
+"""
+
+from PySide6 import QtWidgets
+
+from a_threads import WeatherHandler
+
+
+class Window(QtWidgets.QWidget):
+
+    def __init__(self, parent=None):
+        super().__init__(parent)
+
+        self.lat = 0
+        self.lon = 0
+
+        self.initUi()
+        self.__initSignals()
+        self.weatherHandler = WeatherHandler()
+
+    def initUi(self):
+        """
+        Инициализация Ui
+        :return: None
+        """
+        self.inputLat = QtWidgets.QLineEdit()
+        self.inputLat.setPlaceholderText("Введите широту")
+        self.inputLon = QtWidgets.QLineEdit()
+        self.inputLon.setPlaceholderText("Введите долготу")
+        l_Lat_Lon = QtWidgets.QHBoxLayout()
+        l_Lat_Lon.addWidget(self.inputLat)
+        l_Lat_Lon.addWidget(self.inputLon)
+
+        self.inputDelay = QtWidgets.QLineEdit()
+        self.inputDelay.setPlaceholderText("Введите время задержки")
+
+        self.outputWheather = QtWidgets.QTextEdit()
+
+        self.pushButtonHandle = QtWidgets.QPushButton("Старт")
+        self.pushButtonHandle.setCheckable(True)
+
+        layout = QtWidgets.QVBoxLayout()
+
+        layout.addLayout(l_Lat_Lon)
+        layout.addWidget(self.inputDelay)
+        layout.addWidget(self.outputWheather)
+        layout.addWidget(self.pushButtonHandle)
+
+        self.setLayout(layout)
+        self.setMinimumSize(300, 140)
+
+    def __initSignals(self):
+        self.pushButtonHandle.clicked.connect(self.on_started)
+        self.inputDelay.textChanged.connect(lambda: print(self.inputDelay))
+
+
+        # self.outputWheather.setPlainText(self.weatherHandler.data)
+
+    def on_started(self):
+        self.weatherHandler = WeatherHandler(lat=int(self.inputLat.text()), lon=int(self.inputLon.text()))
+        self.weatherHandler.setDelay(int(self.inputDelay.text()))
+        self.weatherHandler.start()
+        self.weatherHandler.sleep(int(self.inputDelay.text()))
+        self.weatherHandler.connect(self.get_signal_from_thread)
+
+    def get_signal_from_thread(self):
+        if self.pushButtonHandle.isChecked():
+            self.pushButtonHandle.setText('Остановить')
+            self.inputLat.setReadOnly(True)
+            self.inputLon.setReadOnly(True)
+            self.outputWheather.setReadOnly(True)
+        elif not self.pushButtonHandle.isChecked():
+            self.weatherHandler.terminate()
+            self.pushButtonHandle.setText('Запустить')
+            self.inputLat.setReadOnly(False)
+            self.inputLon.setReadOnly(False)
+            self.outputWheather.setReadOnly(False)
+
+
+
+if __name__ == "__main__":
+    app = QtWidgets.QApplication()
+
+    window = Window()
+    window.show()
+
+    app.exec()
